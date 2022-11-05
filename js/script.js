02bb43b33c223b4d3d53a71a0dbd3eb57835528d
apalmer8 <apalmer8@hawk.iit.edu>
2022-11-04 19:11:23 -0500

    Troubleshooting
    Fixed slider not inputting text to a percentage field. Cleaned up UI. Cleaned up CSS

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 document.querySelector("#tip-form").onchange = function () {
   var bill = Number(document.getElementById("billTotal").value);
   var tip = document.getElementById("tipInput").value;
-  document.getElementById("tipOutput").innerHTML = `${tip}%`;
 
+  document.getElementById("tipOutput").value = `${tip}%`;
   var tipVal = bill * (tip / 100);
   var total = bill + tipVal;
   console.log(total);
+  
+  var tipPercent = document.getElementById("tipPercent").value;
+  document.getElementById("tipPercent").value = `${tip}%`;
+
   var tipAmt = document.querySelector("#tipAmount");
   var billPlusTip = document.querySelector("#billPlusTip");
-
+ 
   tipAmt.value = tipVal.toFixed(2);
   billPlusTip.value = total.toFixed(2);
 };
